2fa91becedf343b3352ff5fc139a3ecd060741de
tapfer-abdullah <tapfer.abdullah@gmail.com>
2024-02-24 17:15:58 +0600

    payment field added

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+// next.config.mjs
+const nextConfig = {
+    images: {
+        // domains: ['i.ibb.co', 'example.com', 'another-domain.com'],
+        domains: ['*', 'i.ibb.co'],
+    },
+};
 
 export default nextConfig;
