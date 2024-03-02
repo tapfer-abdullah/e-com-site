501be11fecc316bfa839223f8b4a8df6701ec7de
tapfer-abdullah <tapfer.abdullah@gmail.com>
2024-03-03 03:35:34 +0600

    stipe payment added

--- src/app/Payment/success.html/PaymentSuccessPage.jsx
+++ src/app/Payment/success.html/PaymentSuccessPage.jsx
@@ -1,13 +1,20 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const PaymentSuccessPage = () => {
-  console.log(window.location?.search);
-  const urlParams = new URLSearchParams(window.location?.search);
-  const payment_intent = urlParams.get("payment_intent");
-  const payment_intent_client_secret = urlParams.get("payment_intent_client_secret");
+  const [payment_intent, setPayment_intent] = useState("");
 
-  console.log(payment_intent);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      console.log(window.location?.search);
+      const urlParams = new URLSearchParams(window.location?.search);
+      const payment_intent = urlParams.get("payment_intent");
+      const payment_intent_client_secret = urlParams.get("payment_intent_client_secret");
+      setPayment_intent(payment_intent);
+    }
+  }, []);
+
+  //   console.log(payment_intent);
 
   return (
     <div>
